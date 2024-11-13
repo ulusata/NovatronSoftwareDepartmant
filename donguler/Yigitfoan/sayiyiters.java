b151759c39c9202b97f00c6231a86bf68b6b7cc9
Yiğit Foan <yigitfoan75@gmail.com>
2024-11-13 21:19:12 +0300

    Update sayiyiters.java

--- donguler/Yigitfoan/sayiyiters.java
+++ donguler/Yigitfoan/sayiyiters.java
@@ -6,9 +6,9 @@ public static void main(String[] args){
     System.out.println("Ters çevirmek istediğiniz sayiyi giriniz:");
     int sayi= scanner.nextInt();
     while(sayi>0){
-        
-        sayi/= 10;
         System.out.print(sayi % 10);
+        sayi/= 10;
+
     }
 
 
