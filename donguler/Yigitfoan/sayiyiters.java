9c813981ce17288c43ea4052a78ed383c2eca7f4
Yiğit Foan <yigitfoan75@gmail.com>
2024-11-13 21:09:46 +0300

    Add files via upload

---
+++ donguler/Yigitfoan/sayiyiters.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class sayiyiters{
+public static void main(String[] args){
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Ters çevirmek istediğiniz sayiyi giriniz:");
+    int sayi= scanner.nextInt();
+    while(sayi>0){
+        
+        System.out.print(sayi % 10);
+        sayi/= 10;
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
