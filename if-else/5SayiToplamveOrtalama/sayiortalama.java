60b81cece85bcc441981a9628320454aa52d6e72
Yiğit Foan <yigitfoan75@gmail.com>
2024-12-11 22:45:33 +0300

    Add files via upload

---
+++ if-else/5SayiToplamveOrtalama/sayiortalama.java
@@ -0,0 +1,32 @@
+import java.util.Scanner{
+
+ public class sayıortlama{
+
+ public static void main(String[]args){
+    Scanner ort = new Scanner();
+
+ System.out.println("birinci sayiyi giriniz");
+ int sayi1= input.nextInt();
+
+ System.out.println("ikinci sayiyi giriniz");
+ int sayi2= input.nextInt()
+
+ System.out.println("üçüncü sayiyi giriniz");
+ int sayi3= input.nextInt()
+
+ System.out.println("dördüncü sayiyi giriniz");
+ int sayi4= input.nextInt()
+
+ System.out.println("beşinci sayiyi giriniz");
+ sayi5= input.nextInt()
+
+ int toplam= sayi1 + sayi2 + sayi3 + sayi4 + sayi5
+ system.out.println("Sayilarinizin ortalamasi"+ toplam/5)
+
+
+
+ }
+
+
+
+ }
